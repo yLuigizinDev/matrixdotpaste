e0bb7c2f037b6320b11841f89b2b5f1724df8ddf
FloxxStudio <83787802+yLuigizinDev@users.noreply.github.com>
2022-05-14 19:18:05 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -4251,44 +4251,73 @@ local Multipliers = {
     ["RightFoot"] = 0.75,
 }
 local ChamItems = {}
-local Skyboxes = {
-    ["nebula"] = {
-		SkyboxLf = "rbxassetid://159454286",
-		SkyboxBk = "rbxassetid://159454299",
-		SkyboxDn = "rbxassetid://159454296",
-		SkyboxFt = "rbxassetid://159454293",
-		SkyboxLf = "rbxassetid://159454286",
-		SkyboxRt = "rbxassetid://159454300",
-		SkyboxUp = "rbxassetid://159454288",
-	},
-    ["vaporwave"] = {
-		SkyboxLf = "rbxassetid://1417494402",
-		SkyboxBk = "rbxassetid://1417494030",
-		SkyboxDn = "rbxassetid://1417494146",
-		SkyboxFt = "rbxassetid://1417494253",
-		SkyboxLf = "rbxassetid://1417494402",
-		SkyboxRt = "rbxassetid://1417494499",
-		SkyboxUp = "rbxassetid://1417494643",
-	},
-	["clouds"] = {
-		SkyboxLf = "rbxassetid://570557620",
-		SkyboxBk = "rbxassetid://570557514",
-		SkyboxDn = "rbxassetid://570557775",
-		SkyboxFt = "rbxassetid://570557559",
-		SkyboxLf = "rbxassetid://570557620",
-		SkyboxRt = "rbxassetid://570557672",
-		SkyboxUp = "rbxassetid://570557727",
-	},
-	["twilight"] = {
-		SkyboxLf = "rbxassetid://264909758",
-		SkyboxBk = "rbxassetid://264908339",
-		SkyboxDn = "rbxassetid://264907909",
-		SkyboxFt = "rbxassetid://264909420",
-		SkyboxLf = "rbxassetid://264909758",
-		SkyboxRt = "rbxassetid://264908886",
-		SkyboxUp = "rbxassetid://264907379",
-	},
-}
+
+local Skyboxes = {      
+			["nebula"] = {      
+				SkyboxLf = "rbxassetid://159454286",      
+				SkyboxBk = "rbxassetid://159454299",      
+				SkyboxDn = "rbxassetid://159454296",      
+				SkyboxFt = "rbxassetid://159454293",      
+				SkyboxLf = "rbxassetid://159454286",      
+				SkyboxRt = "rbxassetid://159454300",      
+				SkyboxUp = "rbxassetid://159454288",      
+			},      
+			["vaporwave"] = {      
+				SkyboxLf = "rbxassetid://1417494402",      
+				SkyboxBk = "rbxassetid://1417494030",      
+				SkyboxDn = "rbxassetid://1417494146",      
+				SkyboxFt = "rbxassetid://1417494253",      
+				SkyboxLf = "rbxassetid://1417494402",      
+				SkyboxRt = "rbxassetid://1417494499",      
+				SkyboxUp = "rbxassetid://1417494643",      
+			},      
+			["clouds"] = {      
+				SkyboxLf = "rbxassetid://570557620",      
+				SkyboxBk = "rbxassetid://570557514",      
+				SkyboxDn = "rbxassetid://570557775",      
+				SkyboxFt = "rbxassetid://570557559",      
+				SkyboxLf = "rbxassetid://570557620",      
+				SkyboxRt = "rbxassetid://570557672",      
+				SkyboxUp = "rbxassetid://570557727",      
+			},      
+			["twilight"] = {      
+				SkyboxLf = "rbxassetid://264909758",      
+				SkyboxBk = "rbxassetid://264908339",      
+				SkyboxDn = "rbxassetid://264907909",      
+				SkyboxFt = "rbxassetid://264909420",      
+				SkyboxLf = "rbxassetid://264909758",      
+				SkyboxRt = "rbxassetid://264908886",      
+				SkyboxUp = "rbxassetid://264907379",      
+			},
+			["Dababy"] = {
+				SkyboxLf = "rbxassetid://7245418472",
+				SkyboxBk = "rbxassetid://7245418472",
+				SkyboxDn = "rbxassetid://7245418472",
+				SkyboxFt = "rbxassetid://7245418472",
+				SkyboxLf = "rbxassetid://7245418472",
+				SkyboxRt = "rbxassetid://7245418472",
+				SkyboxUp = "rbxassetid://7245418472",
+			},
+			["MC"] = {
+				SkyboxLf = "rbxassetid://1876543392",
+				SkyboxBk = "rbxassetid://1876545003",
+				SkyboxDn = "rbxassetid://1876544331",
+				SkyboxFt = "rbxassetid://1876542941",
+				SkyboxLf = "rbxassetid://1876543392",
+				SkyboxRt = "rbxassetid://1876543764",
+				SkyboxUp = "rbxassetid://1876544642",
+			},
+			["Chill"] = {
+				SkyboxLf = "rbxassetid://5103948542",
+				SkyboxBk = "rbxassetid://5084575798",
+				SkyboxDn = "rbxassetid://5084575916",
+				SkyboxFt = "rbxassetid://5103949679",
+				SkyboxLf = "rbxassetid://5103948542",
+				SkyboxRt = "rbxassetid://5103948784",
+				SkyboxUp = "rbxassetid://5084576400",
+			
+			}, 
+		}      
 local NewScope
 do
 	local ScreenGui = Instance.new("ScreenGui")
